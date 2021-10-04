276fcc4bd74515772fa097d7bcde4e0814c3faed
Paula Peña Constain <67760493+paulap015@users.noreply.github.com>
2021-10-04 16:41:09 -0500

    Update Leds.h

--- Active_environment/Active_environment/Leds.h
+++ Active_environment/Active_environment/Leds.h
@@ -1,8 +1,20 @@
-/* 
- * File:   Leds.h
- * Author: ASUS
- *
- * Created on 1 de octubre de 2021, 17:49
+/*!
+\file   Leds.h
+\date   2021-10-04
+\author Andrés Muñoz <andresdmunoz@unicauca.edu.co> 104618021296
+        Paula Peña <ppena@unicauca.edu.co> 104618021314
+		Julián Martinez <juliansmartinez@unicauca.edu.co> 104618021321
+\brief  Configuración leds y sus elementos.
+\par Copyright
+Information contained herein is proprietary to and constitutes valuable
+confidential trade secrets of unicauca, and
+is subject to restrictions on use and disclosure.
+\par
+Copyright (c) unicauca 2021. All rights reserved.
+\par
+The copyright notices above do not evidence any actual or
+intended publication of this material.
+ ******************************************************************************
  */
 
 #ifndef LEDS_H
