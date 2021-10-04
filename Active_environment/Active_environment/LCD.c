7f7d2355fcc5d6c71a20a7439e0709e1ba87da07
Julián Santiago Martinez Trullo <67648220+JulianSanti28@users.noreply.github.com>
2021-10-04 16:49:06 -0500

    Update LCD.c

--- Active_environment/Active_environment/LCD.c
+++ Active_environment/Active_environment/LCD.c
@@ -1,5 +1,32 @@
+/*!
+\file   LCD.c
+\date   2021-10-04
+\author Andrés Muñoz <andresdmunoz@unicauca.edu.co> 104618021296
+        Paula Peña <ppena@unicauca.edu.co> 104618021314
+		Julián Martinez <juliansmartinez@unicauca.edu.co> 104618021321
+\brief  Funcionalidades LCD.
+
+\par Copyright
+Information contained herein is proprietary to and constitutes valuable
+confidential trade secrets of unicauca, and
+is subject to restrictions on use and disclosure.
+
+\par
+Copyright (c) unicauca 2021. All rights reserved.
+
+\par
+The copyright notices above do not evidence any actual or
+intended publication of this material.
+ ******************************************************************************
+ */
+
+
 #include "LCD.h"
 
+/*
+*@bref Inicializa el LCD con los valores necesarios para el proyecto
+*@return void
+*/
 void inicializar_lcd() {
     LCD_Port = 0; /*PORT as Output Port*/
     __delay_ms(15); /*15ms,16x2 LCD Power on delay*/
@@ -26,15 +53,21 @@ void LCD_Command(unsigned char cmd) {
     __delay_ms(3);
 }
 
+/*
+*@bref Escribe un dato de tipo char que ingresa como parámetro.
+*@param dat Dato que será escrito en el LCD 
+*@return void
+*/
 void LCD_Char(unsigned char dat) {
-    ldata = (ldata & 0x0f) | (0xF0 & dat); /*Send higher nibble of data first to PORT*/
-    RS = 1; /*Data Register is selected*/
-    EN = 1; /*High-to-low pulse on Enable pin to latch data*/
+    
+    ldata = (ldata & 0x0f) | (0xF0 & dat); /*Envía un nibble mayor de datos primero a PORT*/
+    RS = 1; /*El registro de datos está seleccionado*/
+    EN = 1; /*Pulso de alto a bajo en Activar pin para bloquear datos*/
     NOP();
     EN = 0;
     __delay_ms(1);
-    ldata = (ldata & 0x0f) | (dat << 4); /*Send lower nibble of data to PORT*/
-    EN = 1; /*High-to-low pulse on Enable pin to latch data*/
+    ldata = (ldata & 0x0f) | (dat << 4); /*Envía una pequeña cantidad de datos a PORT*/
+    EN = 1; /*Pulso de alto a bajo en Activar pin para bloquear datos*/
     NOP();
     EN = 0;
     __delay_ms(3);
@@ -42,6 +75,11 @@ void LCD_Char(unsigned char dat) {
 
 }
 
+/*
+*@bref Escribe un mensaje de tipo dirección de memoria char que ingresa como parámetro.
+*@param msg Mensaje que será escrito en el LCD 
+*@return void
+*/
 void LCD_String(const char *msg) {
     while ((*msg) != 0) {
         LCD_Char(*msg);
@@ -50,49 +88,75 @@ void LCD_String(const char *msg) {
 
 }
 
+/*
+*@bref Escribe un mensaje de tipo char que ingresa como parámetro en una posición específica del LCD 16 x 2.
+*@param row Fila en la que será escrito el mensaje en el LCD
+*@param pos Posición dentro de la fila en donde será escrito el mensaje en el LCD 
+*@param msg Mensaje que será escrito en el LCD 
+*@return void
+*/
 void LCD_String_xy(char row, char pos, const char *msg) {
     char location = 0;
     if (row <= 1) {
-        location = (0x80) | ((pos) & 0x0f); /*Print message on 1st row and desired location*/
+        location = (0x80) | ((pos) & 0x0f); /*Imprimir mensaje en la 1ra fila y ubicación deseada*/
         LCD_Command(location);
     } else {
-        location = (0xc0) | ((pos) & 0x0f); /*Print message on 2nd row and desired location*/
+        location = (0xc0) | ((pos) & 0x0f); /*Imprima el mensaje en la segunda fila y la ubicación deseada*/
         LCD_Command(location);
     }
-
 
     LCD_String(msg);
 
 }
 
+/*
+*@bref Permite ubicarse en una posición específica del LCD 16 x 2.
+*@param row Fila en la se requiere la ubicación.
+*@param pos Posición dentro de la fila en donde se requiere la ubicación
+*@return void
+*/
 void LCD_xy(char row, char pos) {
     char location = 0;
     if (row <= 1) {
-        location = (0x80) | ((pos) & 0x0f); /*Print message on 1st row and desired location*/
+        location = (0x80) | ((pos) & 0x0f); /*Imprimir mensaje en la 1ra fila y ubicación deseada*/
         LCD_Command(location);
     } else {
-        location = (0xC0) | ((pos) & 0x0f); /*Print message on 2nd row and desired location*/
+        location = (0xC0) | ((pos) & 0x0f); /*Imprima el mensaje en la segunda fila y la ubicación deseada*/
         LCD_Command(location);
     }
 }
 
+/*
+*@bref Permite limpiar la información del LCD 16 x 2.
+*@return void
+*/
 void LCD_Clear() {
-    LCD_Command(CMD_CLEAR_LCD); /*clear display screen*/
+    LCD_Command(CMD_CLEAR_LCD); /*Limpia la pantalla del display*/
 }
 
+/*
+*@bref Permite mostrar un emoticon guardado previamente en el LCD 16x2.
+*@param i ubicación del emoticón guardado previamente.
+*@return void
+*/
 void mostrar_emoticon(char i) {
 
-    LCD_Command(0x80 | (i * 2));
-    LCD_Char(i);
+    LCD_Command(0x80 | (i * 2)); /*Accede directamente el emoticón con la ubicación dada y la ubica en la primera fila del LCD*/
+    LCD_Char(i);/*Envía el dato para ser mostrado en el LCD*/
 
 }
 
+/*
+*@bref Permite almacenar un emoticon descrito en un arreglo.
+*@param loc Posición dentro de la primera fila del LCD.
+*@return void
+*/
 void LCD_Custom_Char(unsigned char loc, unsigned char *msg) {
 
     unsigned char i;
     if (loc < 8) {
-        LCD_Command(0x40 + (loc * 8)); /* Command 0x40 and onwards forces the device to point CGRAM address */
-        for (i = 0; i < 8; i++) /* Write 8 byte for generation of 1 character */
+        LCD_Command(0x40 + (loc * 8)); /*El comando 0x40 y en adelante obliga al dispositivo a señalar la dirección CGRAM*/
+        for (i = 0; i < 8; i++) /*Escriba 8 bytes para la generación de 1 carácter (emoticon)*/
             LCD_Char(msg[i]);
     }
 
