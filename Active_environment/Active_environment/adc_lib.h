2e48d422bebbe1d6d5f8588258f335d8dca6e4f6
Julián Santiago Martinez Trullo <67648220+JulianSanti28@users.noreply.github.com>
2021-10-04 17:06:25 -0500

    Update adc_lib.h

--- Active_environment/Active_environment/adc_lib.h
+++ Active_environment/Active_environment/adc_lib.h
@@ -1,10 +1,23 @@
-/* 
- * File:   adc_lib.h
- * Author: ASUS
- *
- * Created on 28 de septiembre de 2021, 12:38
+/*!
+\file   adc_lib.h
+\date   2021-10-04
+\author Andrés Muñoz <andresdmunoz@unicauca.edu.co> 104618021296
+        Paula Peña <ppena@unicauca.edu.co> 104618021314
+		Julián Martinez <juliansmartinez@unicauca.edu.co> 104618021321
+\brief  Archivo de cabacera del archivo adc_lib.c
+\par Copyright
+Information contained herein is proprietary to and constitutes valuable
+confidential trade secrets of unicauca, and
+is subject to restrictions on use and disclosure.
+\par
+Copyright (c) unicauca 2021. All rights reserved.
+\par
+The copyright notices above do not evidence any actual or
+intended publication of this material.
+ ******************************************************************************
  */
 
+/*Definiciones para una mejor comprensión del código*/
 #define HACER_CONVERSION ADCON0bits.GO_DONE
 #define RESULTADO ADRESH
 #define CANAL_0 0b0000
@@ -30,13 +43,9 @@
 #ifdef	__cplusplus
 extern "C" {
 #endif
-    
-    void adc_init8Bits(int canal); 
-    unsigned char adc_read8Bits();
-    
+    /*Inicialización de las funciones utilizadas*/
     void adc_init10Bits(int canal);  
     unsigned int adc_read10Bits();  
-    
     void adc_config(); 
     
 #ifdef	__cplusplus
