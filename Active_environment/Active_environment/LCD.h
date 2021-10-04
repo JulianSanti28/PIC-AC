dc0ccb172e2ca995f24237212bfe23bdfb7ddec9
Julián Santiago Martinez Trullo <67648220+JulianSanti28@users.noreply.github.com>
2021-10-04 16:54:29 -0500

    Update LCD.h

--- Active_environment/Active_environment/LCD.h
+++ Active_environment/Active_environment/LCD.h
@@ -1,8 +1,23 @@
-/* 
- * File:   LCD.h
- * Author: ASUS
- *
- * Created on 29 de septiembre de 2021, 13:43
+/*!
+\file   LCD.h
+\date   2021-10-04
+\author Andrés Muñoz <andresdmunoz@unicauca.edu.co> 104618021296
+        Paula Peña <ppena@unicauca.edu.co> 104618021314
+		Julián Martinez <juliansmartinez@unicauca.edu.co> 104618021321
+\brief  Archivo de cabecera de LCD.c.
+
+\par Copyright
+Information contained herein is proprietary to and constitutes valuable
+confidential trade secrets of unicauca, and
+is subject to restrictions on use and disclosure.
+
+\par
+Copyright (c) unicauca 2021. All rights reserved.
+
+\par
+The copyright notices above do not evidence any actual or
+intended publication of this material.
+ ******************************************************************************
  */
 
 #ifndef LCD_H
@@ -12,26 +27,26 @@
 extern "C" {
 #endif
 
-#include <xc.h> // include processor files - each processor file is guarded.  
-#include <pic18f4550.h>
-#include "fuses.h"
+#include <xc.h> //incluir archivos de procesador: cada archivo de procesador está protegido.  
+#include <pic18f4550.h> //Incluir la librería del PIC18F4550
+#include "fuses.h" //Incluye la configuración general del PIC
 
-    /********Definition of Ports***********/
+    /***********************Definición de puertos*******************************/
 
-#define RS LATD0                    /*PIN 0 of PORTB is assigned for register select Pin of LCD*/
-#define EN LATD1                    /*PIN 1 of PORTB is assigned for enable Pin of LCD */
-#define ldata LATD                  /*PORTB(PB4-PB7) is assigned for LCD Data Output*/ 
+#define RS LATD0                    /*El PIN 0 de PORTB se asigna para el pin de selección de registro de la pantalla LCD*/
+#define EN LATD1                    /*El PIN 1 de PORTB se asigna para habilitar el Pin de la pantalla LCD*/
+#define ldata LATD                  /*PORTB (PB4-PB7) está asignado para salida de datos LCD*/ 
 #define LCD_Port TRISD   
 #define CMD_CLEAR_LCD 0x01
     
-void inicializar_lcd();                    /*Initialize LCD*/
-void LCD_Command(unsigned char );   /*Send command to LCD*/
-void LCD_Char(unsigned char x);     /*Send data to LCD*/
-void LCD_String(const char * );      /*Display data string on LCD*/
+void inicializar_lcd();             /*Inicializa el LCD*/
+void LCD_Command(unsigned char );   /*Enviar comando a la pantalla LCD*/
+void LCD_Char(unsigned char x);     /*Enviar datos a la pantalla LCD*/
+void LCD_String(const char * );     /*Mostrar cadena de datos en la pantalla LCD*/
 void LCD_String_xy(char, char , const char *);
-void LCD_Clear();/*Clear LCD Screen*/
-void LCD_Custom_Char(unsigned char loc, unsigned char *msg);
-void mostrar_emoticon(char i);
+void LCD_Clear();/*Limpiar pantalla del LCD*/
+void LCD_Custom_Char(unsigned char loc, unsigned char *msg); /*Función para escribir en el LCD */
+void mostrar_emoticon(char i);/*Función para mostrar un emoticón*/
 
 
 
